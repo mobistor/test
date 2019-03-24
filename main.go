e90d37abc86866fa219b9c03f392538bdf843ef2
root <jack@mobistor.com>
2019-03-24 07:08:38 +0000

    test

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 import (
+	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 func main() {
+	fmt.Println("test")
 	r := gin.Default()
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"alive": true})
